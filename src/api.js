88d4b13977aca13b49b4c9813a417e2f0453e1a2
Erik Grubbs <erikgrubbs@gmail.com>
2021-09-09 03:13:38 -0700

    removes console log

--- src/api.js
+++ src/api.js
@@ -2,6 +2,5 @@ const API_URL = 'http://localhost:8000';
 
 export const getAllPlayers = async () => {
   const response = await fetch(`${API_URL}/players`);
-  console.log(response)
   return response.json();
 }
